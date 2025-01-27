b2c4bfc3b832be69eeb62795a39aa1780dd5e185
gaelrf <gaelrey99@gmail.com>
2025-01-27 20:10:23 +0100

    tienda inicio

--- tienda.php
+++ tienda.php
@@ -1,11 +1,40 @@
+<?php
+
+session_start();
+if (!isset($_SESSION["usuario_id"])) {
+    header("Location: index");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tienda</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/tienda.css">
 </head>
+
 <body>
-    <h1>Tienda</h1>
+    <header>
+        <i class="fa-solid fa-store"></i>
+        <p>
+            Bienvenido <?php echo $_SESSION['usuario']; ?>
+            <a href="logout">Cerrar Sesión</a>
+        </p>
+    </header>
+    <main>
+        <aside>
+        </aside>
+        <section>
+        </section>
+    </main>
+    <footer>
+    </footer>
 </body>
+
 </html>
