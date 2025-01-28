4e51bd9148bfcadd766deaab381f550fc435d7d7
gaelrf <gaelrey99@gmail.com>
2025-01-28 16:53:54 +0100

    footer

---
+++ partials/aside.php
@@ -0,0 +1,11 @@
+<aside>
+    <ul>
+        <li><a href="tienda"><i class="fa-solid fa-store"></i> Tienda</a></li>
+        <li><a href="proovedores"><i class="fa-solid fa-truck-field"></i> Proovedores</a></li>
+        <li><a href="clientes"><i class="fa-solid fa-person"></i> Clientes</a></li>
+        <li><a href="categorias"><i class="fa-solid fa-layer-group"></i> Categorías</a></li>
+        <li><a href="productos"><i class="fa-solid fa-boxes-stacked"></i> Productos</a></li>
+        <li><a href="ventas"><i class="fa-solid fa-cash-register"></i> Ventas</a></li>
+
+    </ul>
+</aside>
